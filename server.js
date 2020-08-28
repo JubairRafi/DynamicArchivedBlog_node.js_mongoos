e3c1992c0694dbdcca2323332f34c9a639ee0dce
jobairRafi96 <jubairrafi96960@gmail.com>
2020-08-28 11:22:07 -0700

    basic page structure done

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 
 const app = express()
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
 
 app.get("/",(req,res)=>{
-  res.send("hello")
+  res.render("index")
 })
-
+app.get("/contact",(req,res)=>{
+  res.render("contact")
+})
+app.get("/about",(req,res)=>{
+  res.render("about")
+})
+app.get("/archive",(req,res)=>{
+  res.render("archive")
+})
 app.listen(3000, err=>{
   if(err)console.log(err)
   else console.log("ss")
